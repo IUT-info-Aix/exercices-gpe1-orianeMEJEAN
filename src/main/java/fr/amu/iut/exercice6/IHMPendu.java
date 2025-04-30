1f053798a4e2526bccc0bc41884ac314fa7693e2
IUT-Oriane <mejean.oriane@sfr.fr>
2025-04-30 18:36:32 +0200

    Pendu final

--- src/main/java/fr/amu/iut/exercice6/IHMPendu.java
+++ src/main/java/fr/amu/iut/exercice6/IHMPendu.java
@@ -2,55 +2,50 @@ package fr.amu.iut.exercice6;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class IHMPendu extends Application
 {
     private String motADeviner;
-    private int nbVie = 7;
-    private StringBuilder motCache = new StringBuilder();
-
+    private int nbVie;
+    private StringBuilder motCache;
     private Label labelVies;
     private Label labelMot;
+    private ImageView imagePendu;
+    private VBox touchesClavier;
+    private Button boutonRejouer;
+    private Dico dico;
+    private boolean partieFinie;
 
     @Override
     public void start(Stage primaryStage)
     {
-        // Récupération du mot à deviner
-        Dico dico = new Dico();
-        motADeviner = dico.getMot().toUpperCase();
+        dico = new Dico();
+        labelVies = new Label();
+        labelMot = new Label();
+        imagePendu = new ImageView();
+        touchesClavier = new VBox(5);
+        boutonRejouer = new Button("Rejouer");
 
-        // Initialiser le mot caché avec des étoiles
-        for (int i = 0; i < motADeviner.length(); i++)
-        {
-            motCache.append("*");
-        }
+        VBox vBox = new VBox(15);
+        vBox.setSpacing(15);
+        vBox.setStyle("-fx-alignment: center; -fx-background-color: #cddde4;");
 
-        // Ajout du Champs d'écrit & du Label des vies
-        TextField champLettre = new TextField();
-        champLettre.setPromptText("Proposez une lettre");
-        champLettre.setMaxWidth(150);
+        debutPartie();
 
-        labelVies = new Label("Nombre de vies : " + nbVie);
-        labelMot = new Label(motCache.toString());
+        boutonRejouer.setOnAction(e -> debutPartie());
 
-        champLettre.setOnAction(event ->
-        {
-            String input = champLettre.getText().toUpperCase();
-            if (input.length() == 1 && Character.isLetter(input.charAt(0)))
-            {
-                traitementDesLettre(input.charAt(0), dico);
-            }
-            champLettre.clear();
-        });
+        vBox.getChildren().addAll(imagePendu, labelVies, labelMot, touchesClavier, boutonRejouer);
 
-        VBox vbox = new VBox(10, champLettre, labelVies, labelMot);
-        vbox.setStyle("-fx-padding: 20; -fx-alignment: center;");
-
-        Scene scene = new Scene(vbox);
+        Scene scene = new Scene(vBox);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Jeu du pendu");
         primaryStage.setWidth(500);
@@ -58,7 +53,70 @@ public class IHMPendu extends Application
         primaryStage.show();
     }
 
-    private void traitementDesLettre(char lettre, Dico dico)
+    // Debut de game
+    private void debutPartie()
+    {
+        motADeviner = dico.getMot().toUpperCase();
+        nbVie = 7;
+        partieFinie = false;
+
+        motCache = new StringBuilder();
+        for (int i = 0; i < motADeviner.length(); i++)
+        {
+            motCache.append("*");
+        }
+
+        // Zone de Vie
+        labelVies.setText("Nombre de vies : " + nbVie);
+        labelVies.setStyle("-fx-font-size: 20");
+
+        // Zone pour le mot a deviner
+        labelMot.setText(motCache.toString());
+        labelMot.setStyle("-fx-font-size: 20");
+
+        // Changement de image quand il y a 1 faute
+        mettreAJourImage();
+
+        // On efface la clavier avant de le remettre (sinon on la en double)
+        touchesClavier.getChildren().clear();
+        creerTouchesClavier();
+    }
+
+    // Création du clavier
+    private void creerTouchesClavier()
+    {
+        // On crée une Hbox avec 1 button pour chaque lettre
+        HBox ligne = new HBox(5);
+        for (char lettre = 'A'; lettre <= 'Z'; lettre++)
+        {
+            Button bouton = new Button(String.valueOf(lettre));
+            bouton.setPrefWidth(40);
+
+            // On crée l'evenement quand on clique sur un bouton
+            bouton.setOnAction(o ->
+            {
+                if (partieFinie) return; // On renvoie rien car la partie est fini (sinon le nombre de vie va dans le négatif)
+
+                char lettreChoisie = bouton.getText().charAt(0);
+                traiterLettre(lettreChoisie);
+
+                bouton.setOpacity(0.3); // On change l'opacité
+            });
+
+            ligne.getChildren().add(bouton);
+
+            // On ajoute 7 bouton max sur chaque ligne
+            if ((lettre - 'A' + 1) % 7 == 0 || lettre == 'Z')
+            {
+                touchesClavier.getChildren().add(ligne);
+                ligne = new HBox(5);
+            }
+            ligne.setStyle("-fx-alignment: center;");
+        }
+    }
+
+    // On verifie si c'est une bonne lettre ou non
+    private void traiterLettre(char lettre)
     {
         boolean bonneLettre = false;
         for (int index : dico.getPositions(lettre, motADeviner))
@@ -74,13 +132,35 @@ public class IHMPendu extends Application
 
         labelMot.setText(motCache.toString());
         labelVies.setText("Nombre de vies : " + nbVie);
+        mettreAJourImage();
 
-        if (nbVie == 0) {
+        if (nbVie == 0)
+        {
             labelVies.setText("Tu as été pendu... Le mot était : " + motADeviner);
+            partieFinie = true;
         }
         else if (motCache.toString().equals(motADeviner))
         {
-            labelVies.setText("Bravo ! Tu as évité ton destin funeste");
+            labelVies.setText("Bravo ! Tu as évité ton destin funeste !");
+            partieFinie = true;
+        }
+    }
+
+    private void mettreAJourImage()
+    {
+        String cheminImage = "exercice6/pendu" + (0 + nbVie) + ".png";
+        ImageView nouvelleImage = new ImageView(cheminImage);
+        nouvelleImage.setFitHeight(200);
+        nouvelleImage.setPreserveRatio(true);
+
+        // Remplacer l'image dans l'imageView
+        imagePendu.setImage(nouvelleImage.getImage());
+
+        if (motCache.toString().equals(motADeviner))
+        {
+            cheminImage = "exercice6/penduWin.png";
+            ImageView win = new ImageView(cheminImage);
+            imagePendu.setImage(win.getImage());
         }
     }
 
