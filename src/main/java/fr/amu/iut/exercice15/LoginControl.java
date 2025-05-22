8e912621fe4996cb63a604051ba5d7c14e4ec085
IUT-Oriane <mejean.oriane@sfr.fr>
2025-05-22 15:04:33 +0200

    Exercice 15, terminée

--- src/main/java/fr/amu/iut/exercice15/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice15/LoginControl.java
@@ -1,32 +1,85 @@
-package fr.amu.iut.exercice5;
+package fr.amu.iut.exercice15;
 
+import javafx.beans.binding.BooleanBinding;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
-public class LoginControl extends GridPane {
+public class LoginControl extends GridPane
+{
     @FXML
     private TextField userId;
 
     @FXML
     private PasswordField pwd;
 
-    private void createBindings() {
-        // MÉTHODE À COMPLÉTER
+    @FXML
+    private Button okBtn;
+
+    @FXML
+    private Button cancelBtn;
+
+    // Controleur de la class
+    public void initialize()
+    {
+        createBindings();
+    }
+
+    private boolean isPasswordValid(String password)
+    {
+        // Mdp vide ou inférieur a 8 == ok non clickable
+        if (password == null || password.length() < 8)
+        {
+            return false;
+        }
+
+        boolean upperCase = password.chars().anyMatch(Character::isUpperCase);
+        boolean digit = password.chars().anyMatch(Character::isDigit);
+        return upperCase && digit;
+    }
+
+    private void createBindings()
+    {
+        // Mdp non editable si id user == 6 carac
+        pwd.editableProperty().bind(userId.textProperty().length().lessThan(6).not());
+
+        // Cancel non cliquable si 2 champs vide
+        cancelBtn.disableProperty().bind(userId.textProperty().isEmpty().and(pwd.textProperty().isEmpty()));
+
+        // Ok non cliquable si mdp n’a pas au moins 8 caractères
+        okBtn.disableProperty().bind(
+                pwd.textProperty().isEmpty().or(
+                        new BooleanBinding()
+                        {
+                            {
+                                super.bind(pwd.textProperty());
+                            }
+
+                            @Override
+                            protected boolean computeValue() {
+                                return !isPasswordValid(pwd.getText());
+                            }
+                        }
+                )
+        );
     }
 
     @FXML
-    private void okClicked() {
+    private void okClicked()
+    {
         System.out.print(userId.getText() + " ");
-        for (char c : pwd.getText().toCharArray()) {
+        for (char c : pwd.getText().toCharArray())
+        {
             System.out.print("*");
         }
         System.out.println();
     }
 
     @FXML
-    private void cancelClicked() {
+    private void cancelClicked()
+    {
         userId.setText("");
         pwd.setText("");
     }
--- src/main/java/fr/amu/iut/exercice15/LoginMain.java
+++ src/main/java/fr/amu/iut/exercice15/LoginMain.java
@@ -1,4 +1,4 @@
-package fr.amu.iut.exercice5;
+package fr.amu.iut.exercice15;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,14 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class LoginMain extends Application {
-    public static void main(String[] args) {
+public class LoginMain extends Application
+{
+    public static void main(String[] args)
+    {
         Application.launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("exercice5/LoginView.fxml"));
+    public void start(Stage stage) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("exercice15/LoginView.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
