9a1ef608315a28ab87b737c0a39d513b529203c8
IUT-Oriane <mejean.oriane@sfr.fr>
2025-05-22 15:54:09 +0200

    Exercice 16, terminée

--- src/main/java/fr/amu/iut/exercice16/ConvertisseurTemperatures.java
+++ src/main/java/fr/amu/iut/exercice16/ConvertisseurTemperatures.java
@@ -1,18 +1,76 @@
-package fr.amu.iut.exercice6;
+package fr.amu.iut.exercice16;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
 
-public class ConvertisseurTemperatures extends Application {
-
+public class ConvertisseurTemperatures extends Application
+{
     @Override
-    public void start(Stage primaryStage) {
-        VBox panneauCelsius = new VBox(30);
-        VBox panneauFahrenheit = new VBox(30);
+    public void start(Stage primaryStage)
+    {
+        // Stockent les valeurs de température
+        DoubleProperty celsius = new SimpleDoubleProperty(0);
+        DoubleProperty fahrenheit = new SimpleDoubleProperty(32);
+
+        // Curseur pour Celsius
+        Slider slideC = new Slider(-18, 100, 0);
+        slideC.setShowTickLabels(true);
+        slideC.setShowTickMarks(true);
+        slideC.setMajorTickUnit(10);
+        slideC.setOrientation(Orientation.VERTICAL);
+        slideC.setPrefHeight(800);
+
+        // Curseur pour Fahrenheit
+        Slider slideF = new Slider(0, 212, 32);
+        slideF.setShowTickLabels(true);
+        slideF.setShowTickMarks(true);
+        slideF.setMajorTickUnit(10);
+        slideF.setOrientation(Orientation.VERTICAL);
+        slideF.setPrefHeight(800);
+
+        // Champs de texte pour afficher les valeurs
+        TextField celsiusF = new TextField();
+        celsiusF.setPrefSize(60, 25);
+
+        TextField fahrenheitF = new TextField();
+        fahrenheitF.setPrefSize(60, 25);
+
+        // Labels pour les unités
+        Label celsiusL = new Label("°C");
+        celsiusL.setStyle("-fx-font-weight: bold");
+
+        Label fahrenheitL = new Label("°F");
+        fahrenheitL.setStyle("-fx-font-weight: bold");
+
+        // Liaison des sliders avec leurs propriétés respectives
+        slideC.valueProperty().bindBidirectional(celsius);
+        slideF.valueProperty().bindBidirectional(fahrenheit);
+
+        // Binding bidirectionnel
+        celsius.addListener((obs, oldVal, newVal) -> fahrenheit.set(newVal.doubleValue() * 9 / 5 + 32));
+        fahrenheit.addListener((obs, oldVal, newVal) -> celsius.set((newVal.doubleValue() - 32) * 5 / 9));
+
+        // Liaison bidirectionnelle
+        Bindings.bindBidirectional(celsiusF.textProperty(), slideC.valueProperty(), new NumberStringConverter());
+        Bindings.bindBidirectional(fahrenheitF.textProperty(), slideF.valueProperty(), new NumberStringConverter());
+
+        VBox panneauCelsius = new VBox(10, celsiusL, slideC, celsiusF);
+        panneauCelsius.setAlignment(Pos.CENTER);
+        VBox panneauFahrenheit = new VBox(10, fahrenheitL, slideF, fahrenheitF);
+        panneauFahrenheit.setAlignment(Pos.CENTER);
 
         HBox root = new HBox(30, panneauCelsius, panneauFahrenheit);
         root.setPadding(new Insets(20));
@@ -21,4 +79,8 @@ public class ConvertisseurTemperatures extends Application {
         primaryStage.show();
     }
 
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
 }
