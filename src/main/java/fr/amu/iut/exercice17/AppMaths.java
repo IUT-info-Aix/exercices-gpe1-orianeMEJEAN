b14f672e5d85f1aca787b7cfca2caec70a8f4f8a
IUT-Oriane <mejean.oriane@sfr.fr>
2025-05-22 16:12:39 +0200

    Code Init

---
+++ src/main/java/fr/amu/iut/exercice17/AppMaths.java
@@ -0,0 +1,19 @@
+package fr.amu.iut.exercice17;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppMaths extends Application
+{
+    @Override
+    public void start(Stage primaryStage)
+    {
+
+        throw new RuntimeException("Méthode à implémenter");
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
